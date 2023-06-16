7f1a51fdb8e81d2e77b8e0ebec82c6877cc27b7a
Stefan Gatu <stefan.gatu@gmail.com>
2023-06-16 20:03:58 +0100

    first commit

--- jobs/hello_rust.groovy
+++ jobs/hello_rust.groovy
@@ -19,7 +19,7 @@ pipeline {
     stages {
         stage('Stage 1') {
             steps {
-                echo 'Hello world!' 
+                echo 'Hello world! K ${git_key_id}' 
             }
         }
     }
