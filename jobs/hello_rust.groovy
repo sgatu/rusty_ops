279b772a993165f40f161084a1b7f55f6c09eb57
Stefan Gatu <stefan.gatu@gmail.com>
2023-06-16 21:12:20 +0100

    first commit

--- jobs/hello_rust.groovy
+++ jobs/hello_rust.groovy
@@ -1,14 +1,15 @@
-
 pipelineJob('hello-rust') {
   description = 'Deploy hello-rust on eks cluster.'
   definition {
     cpsScm {
-        git {
-          remote {
-            url('git@github.com:sgatu/hello_rust.git')
-            credentials("${git_key_id}")
-          }
-          branch('*/main')
+        scm {
+            git {
+                remote {
+                    url('git@github.com:sgatu/hello_rust.git')
+                    credentials("${git_key_id}")
+                }
+                branch('*/main')
+            }
         }
         scriptPath('Jenkinsfile')
         lightweight()
